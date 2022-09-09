d737de64c8be8d8093d31e5f649cf5aebf1257cb
johnlombardi389 <lombardij38@gmail.com>
2022-09-09 18:28:25 -0400

    Added event listener on each cell when game starts

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#statusText");
 const restartBtn = document.querySelector("#restartBtn");
+
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+
+function initializeGame() {
+  cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+}
+initializeGame();
+
+function cellClicked() {}
+
+function updateCell(cell, index) {}
+
+function changePlayer() {}
+
+function checkWinner() {}
+
+function restartGame() {}
