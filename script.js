5750776414c214c6565f236945ae2ee165ee643c
johnlombardi389 <lombardij38@gmail.com>
2022-09-09 19:00:59 -0400

    Added function to check for winner

--- script.js
+++ script.js
@@ -46,6 +46,33 @@ function changePlayer() {
   statusText.textContent = `${currentPlayer}'s turn`;
 }
 
-function checkWinner() {}
+function checkWinner() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i];
+    const cellA = options[condition[0]];
+    const cellB = options[condition[1]];
+    const cellC = options[condition[2]];
+
+    if (cellA === "" || cellB === "" || cellC === "") {
+      continue;
+    }
+    if (cellA === cellB && cellB === cellC) {
+      roundWon = true;
+      break;
+    }
+  }
+
+  if (roundWon) {
+    statusText.textContent = `${currentPlayer} wins!`;
+    running = false;
+  } else if (!options.includes("")) {
+    statusText.textContent = `Tie game!`;
+    running = false;
+  } else {
+    changePlayer();
+  }
+}
 
 function restartGame() {}
